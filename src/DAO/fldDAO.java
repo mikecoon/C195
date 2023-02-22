3c5cb114a5e0b8d29454092784594c74cdf47ea8
mikecoon <michaelcoon02@gmail.com>
2023-02-22 13:28:29 -0700

    fldDAO created.

--- src/DAO/fldDAO.java
+++ src/DAO/fldDAO.java
@@ -1,5 +1,12 @@
 package DAO;
+import helper.JDBC;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import model.firstLevelDivision;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class fldDAO extends firstLevelDivision{
 
@@ -7,5 +14,20 @@ public class fldDAO extends firstLevelDivision{
         super(divisionName, divID, countryID);
     }
 
-    getFLD
+    public static ObservableList<fldDAO> getFLDs() throws SQLException {
+        ObservableList<fldDAO> flds = FXCollections.observableArrayList();
+        String sql = "SELECT * from first_level_divisions";
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()){
+            String divName = rs.getString("Division");
+            int divID = rs.getInt("Division_ID");
+            int countryID = rs.getInt("COUNTRY_ID");
+            fldDAO fld = new fldDAO(divName,divID,countryID);
+            flds.add(fld);
+
+        }
+        return flds;
+    }
 }
